6f9a558660234bfcd55ceaa4c5d8cf839d025195
dfgdfgertt <majinsyn@gmail.com>
2023-06-28 17:39:52 +0700

    update index.html

--- script/converADOCtoHTML.js
+++ script/converADOCtoHTML.js
@@ -1,6 +1,6 @@
 const asciidoctor = require('asciidoctor')();
 
 const adocFile = 'data/index.adoc';
-const htmlFile = 'views/index.html';
+const htmlFile = 'index.html';
 
 asciidoctor.convertFile(adocFile, { to_file: htmlFile });
