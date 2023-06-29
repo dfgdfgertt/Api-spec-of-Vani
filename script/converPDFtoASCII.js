471c9b834e8466061c8b9e732bf53c353b07e7ba
dfgdfgertt <majinsyn@gmail.com>
2023-06-29 17:32:55 +0700

    update

--- script/converPDFtoASCII.js
+++ script/converPDFtoASCII.js
@@ -1,23 +1,34 @@
-const pdftotext = require('pdf-to-text');
 const fs = require('fs');
+const PDFReader = require('pdfjs-dist');
+const asciidoctor = require('asciidoctor.js')();
 
-// Đường dẫn đến tệp PDF
-const pdfPath = 'data/Vanila-API-Spec-V1.1.4.pdf';
-// Đường dẫn đến tệp .adoc để lưu nội dung ASCII
-const adocPath = 'data/output/index.adoc';
+// Đường dẫn đến tệp PDF đầu vào
+const inputPdfPath = 'data/input.pdf';
 
-// Đọc nội dung từ tệp PDF và chuyển đổi sang ASCII
-pdftotext.pdfToText(pdfPath, function(err, data) {
-  if (err) {
-    console.error(err);
-  } else {
-    // Ghi nội dung ASCII vào tệp .adoc
-    fs.writeFile(adocPath, data, function(err) {
-      if (err) {
-        console.error(err);
-      } else {
-        console.log('Đã lưu nội dung vào tệp .adoc');
-      }
-    });
-  }
+// Đường dẫn đến tệp AsciiDoc đầu ra
+const outputAdocPath = 'data/output.adoc';
+
+// Tạo một đối tượng PDFReader để đọc tệp PDF
+const pdfReader = new PDFReader();
+
+// Biến lưu trữ nội dung AsciiDoc
+let asciiDocContent = '';
+
+// Sự kiện được kích hoạt khi có một dòng văn bản được đọc từ PDF
+pdfReader.on('text', (text) => {
+  // Thêm nội dung văn bản vào biến AsciiDoc
+  asciiDocContent += text + '\n';
 });
+
+// Sự kiện được kích hoạt khi kết thúc việc đọc tệp PDF
+pdfReader.on('end', () => {
+  // Tạo tệp AsciiDoc từ nội dung đã thu thập được
+  const asciidoc = asciidoctor.convert(asciiDocContent);
+
+  // Lưu tệp AsciiDoc
+  fs.writeFileSync(outputAdocPath, asciidoc);
+  console.log('Chuyển đổi PDF thành công!');
+});
+
+// Đọc tệp PDF đầu vào
+fs.createReadStream(inputPdfPath).pipe(pdfReader);
